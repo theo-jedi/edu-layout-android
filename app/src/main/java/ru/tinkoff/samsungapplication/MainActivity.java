aa44cd4da303107ef53cb8631ef73d6635e570fa
f.sedov <f.sedov@tinkoff.ru>
2022-11-28 22:09:18 +0300

    Initial commit

---
+++ app/src/main/java/ru/tinkoff/samsungapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.tinkoff.samsungapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
